186ebcb3268de7c5e7c620779bd9720bf555164d
Alex van der Leeuw <alleeuw@gmail.com>
2026-09-09 14:25:57 +0200

    Added a very basic custom manager

--- experimentation/models.py
+++ experimentation/models.py
@@ -4,9 +4,16 @@ class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
 
+class SoloTaskManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(soloTask=True)
+
 class Task(models.Model):
     taskName = models.CharField(max_length=100)
     taskDescription = models.TextField()
     soloTask = models.BooleanField()
     taskLocation = models.CharField(max_length=300)
     taskDate = models.DateField()
+
+    objects = models.Manager()
+    soloTasks = SoloTaskManager()
